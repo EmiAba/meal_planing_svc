ddc2119c0e7254164f353e9d4cbb8562ea2f63f6
EmiAba <abadjieva@gmail.com>
2025-11-10 20:40:23 +0200

    feat: add global exception handling with custom ErrorResponse DTO

---
+++ src/main/java/app/web/dto/ErrorResponse.java
@@ -0,0 +1,19 @@
+package app.web.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+public class ErrorResponse {
+    private LocalDateTime timestamp;
+
+    private String message;
+
+    public ErrorResponse(LocalDateTime timestamp, String message) {
+        this.message = message;
+        this.timestamp = LocalDateTime.now();
+    }
+}
