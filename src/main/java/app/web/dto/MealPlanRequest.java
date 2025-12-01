bb7db7bab8c64373e6394ba2006b34149b9e1b53
EmiAba <abadjieva@gmail.com>
2025-12-01 12:34:29 +0200

    fix: add constructors to MealPlanRequest

--- src/main/java/app/web/dto/MealPlanRequest.java
+++ src/main/java/app/web/dto/MealPlanRequest.java
@@ -4,14 +4,18 @@ import app.model.MealType;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.FutureOrPresent;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 import java.util.UUID;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class MealPlanRequest {
 
     @NotNull(message = "User ID is required")
